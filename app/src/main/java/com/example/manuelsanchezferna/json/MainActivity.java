93052db27656b0e2fc9082dcc54d7949e2f4f893
DjManko <manuelsancfer@gmail.com>
2017-12-26 04:35:56 +0100

    v2-3

--- app/src/main/java/com/example/manuelsanchezferna/json/MainActivity.java
+++ app/src/main/java/com/example/manuelsanchezferna/json/MainActivity.java
@@ -6,11 +6,11 @@ import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.MediaController;
 import android.widget.ProgressBar;
 import android.widget.Spinner;
@@ -28,6 +28,8 @@ import com.google.gson.Gson;
 import com.android.volley.toolbox.Volley;
 import com.google.gson.stream.JsonReader;
 
+
+
 import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
@@ -36,8 +38,13 @@ public class MainActivity extends AppCompatActivity {
     private View linia_vid;
     private ProgressBar progressBar;
     private VideoView videoView;
+    private MediaPlayer mediaPlayer;
 
-    int videosid[] = {R.id.vid0, R.id.vid1, R.id.vid2};  //rellenar
+    int videosid[] = {R.id.vid0, R.id.vid1, R.id.vid2, R.id.vid3, R.id.vid4, R.id.vid5, R.id.vid6,
+            R.id.vid7, R.id.vid8, R.id.vid9};  //rellenar
+    int scoreid[] = {};
+    int tituloid[] = {};
+    int artistaid[] = {};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -76,6 +83,7 @@ public class MainActivity extends AppCompatActivity {
                 Log.i("app","onNothing");
             }
         });
+
 
     }
 
@@ -171,189 +179,83 @@ public class MainActivity extends AppCompatActivity {
                         ConsultaVideos c = gson.fromJson(response.toString(),ConsultaVideos.class);
 
                         if(c.getSuccess()==1) {
+                            final Uri va = Uri.parse(c.getVideos().get(9).getUrl()); //Prueba!
 
-//                            for (int i=0; i<videosid.length;i++){
-//                                videoView = (VideoView) findViewById(videosid[i]);
-//                                Uri v = Uri.parse(c.getVideos().get(i).getUrl());
-//                                String score = Float.toString(
+                            for (int i=0; i<videosid.length;i++){
+                                videoView = (VideoView) findViewById(videosid[i]);  //Falta Videoview1,2...
+                                Uri v = Uri.parse(c.getVideos().get(i).getUrl());
+
+
+//                                TextView score = (TextView) findViewById(scoreid[i]);
+//                                String scoret = Float.toString(
 //                                        c.getVideos().get(i).getScore());
-//                                textView.setText(score);   //para meter el score
-//
-//                                TextView vid0_name = (TextView) findViewById(R.id.vid0_name);
-//                                TextView vid0_art = (TextView) findViewById(R.id.vid0_art);
-//
-//                                vid0_name.setText(c.getVideos().get(0).getTittle());
-//                                vid0_art.setText(c.getVideos().get(0).getName());
-//
-//                                videoView.setVideoURI(v);
-//                            }
+//                                score.setText(scoret);   //Falta R.id...
 
-                            //LISTA DE 10 VIDEOS
+                                //TextView vid0_name = (TextView) findViewById(tituloid[i]);Faltan R.id..
+                                //TextView vid0_art = (TextView) findViewById(artistaid[i]);Faltan R.id..
 
-                            videoView = (VideoView) findViewById(R.id.vid0);
-                            Uri v0 = Uri.parse(c.getVideos().get(0).getUrl());
-                            String score0 = Float.toString(
-                                        c.getVideos().get(0).getScore());
-                                textView.setText(score0);   //para meter el score
+                                //vid0_name.setText(c.getVideos().get(0).getTittle()); idem
+                                //vid0_art.setText(c.getVideos().get(0).getName());
 
-                            TextView vid0_name = (TextView) findViewById(R.id.vid0_name);
-                            TextView vid0_art = (TextView) findViewById(R.id.vid0_art);
-
-                            vid0_name.setText(c.getVideos().get(0).getTittle());
-                            vid0_art.setText(c.getVideos().get(0).getName());
-
-          //                  videoView.setVideoURI(v0);
+                                //videoView.setVideoURI(v);
+                            }
 
 
-                            videoView = (VideoView) findViewById(R.id.vid1);
-                            Uri v1 = Uri.parse(c.getVideos().get(1).getUrl());
-                            String score1 = Float.toString(
-                                    c.getVideos().get(1).getScore());
-                            textView.setText(score1);
+                            videoView.setOnTouchListener(new View.OnTouchListener()
+                            {
+                                @Override
+                                public boolean onTouch(View v, MotionEvent motionEvent)
+                                {
+                                    if (videoView.isPlaying())
+                                    {
+                                        Toast.makeText(getApplicationContext(),
+                                                "touch", Toast.LENGTH_LONG).show();
+                                        videoView.pause();
+//                    if (!getActivity().getActionBar().isShowing())
+//                    {
+//                        getActivity().getActionBar().show();
+//                        mMediaController.show(0);
+//                    }
+//                    position = mVideoView.getCurrentPosition();
+                                        return false;
+                                    }
+                                    else
+                                    { Toast.makeText(getApplicationContext(),
+                                            "touchi", Toast.LENGTH_LONG).show();
+//                    if (getActivity().getActionBar().isShowing())
+//                    {
+//                        getActivity().getActionBar().hide();
+//                        mMediaController.hide();
+//                    }
+//                    videoView.seekTo(position);
+                                        videoView.setVideoURI(va);  //prueba
+                                        return false;
 
-                            /*TextView vid1_name = (TextView) findViewById(R.id.vid1_name);
-                            TextView vid1_art = (TextView) findViewById(R.id.vid1_art);
+                                    }
 
-                            vid_name.setText(c.getVideos().get(1).getTittle());
-                            vid1_art.setText(c.getVideos().get(1).getName());*/
-
-
-        //                    videoView.setVideoURI(v1);
-
-                            videoView = (VideoView) findViewById(R.id.vid2);
-                            Uri v2 = Uri.parse(c.getVideos().get(2).getUrl());
-                            String score2 = Float.toString(
-                                    c.getVideos().get(2).getScore());
-                            textView.setText(score2);
-
-                            /*TextView vid2_name = (TextView) findViewById(R.id.vid2_name);
-                            TextView vid2_art = (TextView) findViewById(R.id.vid2_art);
-
-                            vid1_name.setText(c.getVideos().get(2).getTittle());
-                            vid1_art.setText(c.getVideos().get(2).getName());*/
-
-      //                      videoView.setVideoURI(v2);
-
-
-                            videoView = (VideoView) findViewById(R.id.vid3);
-                            Uri v3 = Uri.parse(c.getVideos().get(3).getUrl());
-                            String score3 = Float.toString(
-                                    c.getVideos().get(3).getScore());
-                            textView.setText(score3);
-
-                            /*TextView vid3_name = (TextView) findViewById(R.id.vid3_name);
-                            TextView vid3_art = (TextView) findViewById(R.id.vid3_art);
-
-                            vid3_name.setText(c.getVideos().get(3).getTittle());
-                            vid3_art.setText(c.getVideos().get(3).getName());*/
-
-        //                    videoView.setVideoURI(v3);
+                                }});
 
 
-                            videoView = (VideoView) findViewById(R.id.vid4);
-                            Uri v4 = Uri.parse(c.getVideos().get(4).getUrl());
-                            String score4 = Float.toString(
-                                    c.getVideos().get(4).getScore());
-                            textView.setText(score4);
 
-                            /*TextView vid4_name = (TextView) findViewById(R.id.vid4_name);
-                            TextView vid4_art = (TextView) findViewById(R.id.vid4_art);
-
-                            vid3_name.setText(c.getVideos().get(4).getTittle());
-                            vid3_art.setText(c.getVideos().get(4).getName());*/
-
-         //                   videoView.setVideoURI(v4);
-
-
-                            videoView = (VideoView) findViewById(R.id.vid5);
-                            Uri v5 = Uri.parse(c.getVideos().get(5).getUrl());
-                            String score5 = Float.toString(
-                                    c.getVideos().get(5).getScore());
-                            textView.setText(score5);
-
-                            /*TextView vid5_name = (TextView) findViewById(R.id.vid5_name);
-                            TextView vid5_art = (TextView) findViewById(R.id.vid5_art);
-
-                            vid3_name.setText(c.getVideos().get(5).getTittle());
-                            vid3_art.setText(c.getVideos().get(5).getName());*/
-
-   //                         videoView.setVideoURI(v5);
-
-
-                            videoView = (VideoView) findViewById(R.id.vid6);
-                            Uri v6 = Uri.parse(c.getVideos().get(6).getUrl());
-                            String score6 = Float.toString(
-                                    c.getVideos().get(6).getScore());
-    //                        textView.setText(score6);
-
-                            /*TextView vid6_name = (TextView) findViewById(R.id.vid6_name);
-                            TextView vid6_art = (TextView) findViewById(R.id.vid6_art);
-
-                            vid6_name.setText(c.getVideos().get(6).getTittle());
-                            vid6_art.setText(c.getVideos().get(6).getName());*/
-
-//                            videoView.setVideoURI(v6);
-
-
-                            videoView = (VideoView) findViewById(R.id.vid7);
-                            Uri v7 = Uri.parse(c.getVideos().get(7).getUrl());
-                            String score7 = Float.toString(
-                                    c.getVideos().get(7).getScore());
-                            textView.setText(score7);
-
-                            /*TextView vid7_name = (TextView) findViewById(R.id.vid7_name);
-                            TextView vid7_art = (TextView) findViewById(R.id.vid7_art);
-
-                            vid7_name.setText(c.getVideos().get(7).getTittle());
-                            vid7_art.setText(c.getVideos().get(7).getName());*/
-
-       //                     videoView.setVideoURI(v7);
-
-
-                            videoView = (VideoView) findViewById(R.id.vid8);
-                            Uri v8 = Uri.parse(c.getVideos().get(8).getUrl());
-                            String score8 = Float.toString(
-                                    c.getVideos().get(8).getScore());
-                            textView.setText(score8);
-
-                            /*TextView vid8_name = (TextView) findViewById(R.id.vid8_name);
-                            TextView vid8_art = (TextView) findViewById(R.id.vid8_art);
-
-                            vid8_name.setText(c.getVideos().get(8).getTittle());
-                            vid8_art.setText(c.getVideos().get(8).getName());*/
-
-        //                   videoView.setVideoURI(v8);
-
-
-                            videoView = (VideoView) findViewById(R.id.vid9);
-                            Uri v9 = Uri.parse(c.getVideos().get(9).getUrl());
-                            String score9 = Float.toString(
-                                    c.getVideos().get(9).getScore());
-         //                   textView.setText(score9);
-
-                            /*TextView vid9_name = (TextView) findViewById(R.id.vid9_name);
-                            TextView vid9_art = (TextView) findViewById(R.id.vid9_art);
-
-                            vid9_name.setText(c.getVideos().get(9).getTittle());
-                            vid9_art.setText(c.getVideos().get(9).getName());*/
-
-         //                   videoView.setVideoURI(v9);
-                            videoView.setVideoURI(v9);
-                            /*MediaController controller = new MediaController(this);
-                            v9.setMediaController(controller);
-                            player.requestFocus();
-                            player.start();
-                            controller.show(10000);*/
-
+                            //TODO conseguir que funcionen estas 4 lineas:
+                            MediaController mediaController = new MediaController(this);
+                            VideoView simpleVideoView = (VideoView) findViewById(R.id.vid9);
+                            simpleVideoView.setMediaController(mediaController);
+                            videoView.setVideoURI(va);
 
 
                             videoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
                                 @Override
                                 public void onPrepared(MediaPlayer mp) {
-                                    mp.setLooping(true);
                                     videoView.start();
+                                    //videoView.stopPlayback();
                                 }
                            });
+
+
+
+
+
                         }
                         else {
                             /*Toast.makeText(getApplicationContext(),
