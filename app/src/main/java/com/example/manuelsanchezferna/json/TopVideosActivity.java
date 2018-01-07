c14cd3cceea7f7df43eff00b13c8afef4ad277e6
DjManko <manuelsancfer@gmail.com>
2018-01-07 19:06:27 +0100

    v3-4. Spinners Videos

--- app/src/main/java/com/example/manuelsanchezferna/json/TopVideosActivity.java
+++ app/src/main/java/com/example/manuelsanchezferna/json/TopVideosActivity.java
@@ -1,6 +1,7 @@
 package com.example.manuelsanchezferna.json;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,6 +9,10 @@ import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.android.volley.Request;
@@ -41,6 +46,8 @@ public class TopVideosActivity extends AppCompatActivity {
         Toast.makeText(this,"Funciona", Toast.LENGTH_LONG).show();
 
         videosGridVid("https://unguled-flash.000webhostapp.com/Consultas/topvideos.php");
+
+        createSpinner();
     }
 
     private void videosGridVid(String url) {
@@ -99,5 +106,60 @@ public class TopVideosActivity extends AppCompatActivity {
         recyclerView.setAdapter(adapter1);
         pDialogImage2.hide();
 
+    }
+
+    private void createSpinner() {
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.desplegable,android.R.layout.simple_spinner_item);
+
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                Seleccio(i);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {Log.i("app","onNothing");}
+        });
+    }
+
+    private void Seleccio(int pos) {
+
+        if (pos == 0){
+
+        }
+
+        if (pos == 1){
+            //Perfil propio usuario
+            Intent intent = new Intent(this,Perfil.class);
+            startActivity(intent);
+        }
+
+        if(pos == 2){
+            //Lista rep
+        }
+
+        if(pos == 3){
+            //Categorías
+        }
+
+        if (pos == 4){
+            //Agenda
+        }
+
+        if (pos == 5){
+            //Configuración
+            Intent intent = new Intent(this,ConfigUsuario.class);
+            startActivity(intent);
+        }
     }
 }
